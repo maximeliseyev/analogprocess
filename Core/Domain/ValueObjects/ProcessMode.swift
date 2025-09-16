694f9c3f42c615297ec5625dcf6d1dd05b7affdd
maximeliseyev <maxim7eli@gmail.com>
2025-09-16 11:09:36 +0300

    Fix localizations

--- Core/Domain/ValueObjects/ProcessMode.swift
+++ Core/Domain/ValueObjects/ProcessMode.swift
@@ -3,4 +3,13 @@ import Foundation
 enum ProcessMode: String, CaseIterable {
     case developing = "Developing"
     case fixer = "Fixer"
+
+    var localizedName: String {
+        switch self {
+        case .developing:
+            return String(localized: "processModesDeveloping")
+        case .fixer:
+            return String(localized: "processModeFixer")
+        }
+    }
 }
